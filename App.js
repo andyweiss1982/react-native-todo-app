ddb89aab921195d64174d18ec558ab6a649e5fa0
Andy Weiss <andyweiss1982@gmail.com>
2020-01-13 11:17:44 -0500

    move form

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { TasksContextProvider } from './context/TasksContext'
 import TasksList from './components/TasksList'
 import Form from './components/Form'
@@ -7,26 +7,8 @@ import Form from './components/Form'
 export default () => (
   <TasksContextProvider>
     <SafeAreaView>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>My Tasks</Text>
-        <Form />
-      </View>
+      <Form />
       <TasksList />
     </SafeAreaView>
   </TasksContextProvider>
 )
-
-const styles = StyleSheet.create({
-  header: {
-    backgroundColor: 'whitesmoke',
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    borderTopColor: 'gray',
-    borderTopWidth: 1
-  },
-  headerText: {
-    fontSize: 30,
-    marginTop: 20
-  }
-})
--- components/Form.js
+++ components/Form.js
@@ -1,29 +1,46 @@
 import React, { useState, useContext } from 'react'
-import { TextInput, StyleSheet } from 'react-native'
+import { TextInput, View, Text, StyleSheet } from 'react-native'
 import { TasksContext } from '../context/TasksContext'
 
 export default () => {
   const { addTask } = useContext(TasksContext)
   const [description, setDescription] = useState('')
   return(
-    <TextInput
-      style={styles.textInput}
-      onChangeText={text => setDescription(text)}
-      value={description}
-      onSubmitEditing={async () => {
-        if (description) {
-          setDescription('')
-          await addTask(description)
-        }
-      }}
-      returnKeyType="done"
-      placeholder="Add a task..."
-      enablesReturnKeyAutomatically={true}
-    />
+    <View style={styles.header}>
+      <Text style={styles.headerText}>
+        My Tasks
+      </Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => setDescription(text)}
+        value={description}
+        onSubmitEditing={async () => {
+          if (description) {
+            setDescription('')
+            await addTask(description)
+          }
+        }}
+        returnKeyType="done"
+        placeholder="Add a task..."
+        enablesReturnKeyAutomatically={true}
+      />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'whitesmoke',
+    padding: 10,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    borderTopColor: 'gray',
+    borderTopWidth: 1
+  },
+  headerText: {
+    fontSize: 30,
+    marginTop: 20
+  },
   textInput: {
     width: '100%',
     borderColor: 'gray',
@@ -33,4 +50,3 @@ const styles = StyleSheet.create({
     backgroundColor: 'white'
   }
 })
-
